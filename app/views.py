ccb7c2539eec93e1a42657e8f4edc3e3990f5dbb
Anton Myronyuk <antonmyronyuk@gmail.com>
2018-05-26 17:14:42 +0300

    add page (and route) for adding a new note, add links between html pages

--- app/views.py
+++ app/views.py
@@ -3,6 +3,26 @@ from flask import render_template
 from app import app
 
 
+"""
+According to task I decided not to give notes to the render_template 
+function as a parameter. All the notes will be requested dynamically
+with using ajax on a client. Furthermore, I will not use constructions
+like this '{{ }}' and this '{% %}' in HTML page to improve performance
+and follow KISS principle
+"""
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/notes')
+def notes():
+    """
+    Page with notes which are ordered by
+    number of unique words in each note
+    """
+    return render_template('notes.html')
+
+
+@app.route('/add_note')
+def add_note():
+    """
+    Page with form for adding a new note
+    """
+    return render_template('add_note.html')
