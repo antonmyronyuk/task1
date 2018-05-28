dfc56064c8f66a211ab788e5ecc5412618b9289b
Anton Myronyuk <antonmyronyuk@gmail.com>
2018-05-28 16:22:04 +0300

    set case unsensitive while checking unique values, improve checking empty strings

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    # sqlite on localhost, postgres on heroku
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- counter.py
+++ counter.py
@@ -9,15 +9,31 @@ def count_unique_words(text):
     :return: int
     """
 
-    # set contain only unique values
+    words = re.split('\W+', text)
+    print(words)
+    # words list may contain empty string only at the begin or/and
+    # at the end if text has leading or trailing spaces
+    # the fastest way to delete empty string is to check only
+    # first and last element of the list directly by index
+    # words list will contain at least one non-empty string
+    # so conditions below should not crash program
+
+    # first
+    if not words[0]:
+        del words[0]
+
+    # last
+    if not words[-1]:
+        del words[-1]
+
+    # set contain only unique values (we use lower() to ignore case)
     # it is easy and really fast way to count unique values
     unique_words = set(
         # there W means [^a-zA-Z0-9_]  + UNICODE, so strings like
         # '___', 'one_two_three' will be calculated as only one word
-        (word for word in re.split('\W+', text) if word)
+        # also we use generator expression to save more memory
+        (word.lower() for word in words)
     )
-
-    print(re.split('\W+', text))
 
     return len(unique_words)
 
